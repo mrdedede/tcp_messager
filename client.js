0fe48724e439aa0f1c29a1b2e7f445f1cec63d9a
mrdedede <acasf@cin.ufpe.br>
2022-07-16 13:14:33 -0300

    segunda questao

--- client.js
+++ client.js
@@ -1,23 +1,25 @@
 const net = require('net')
 const readline = require('readline')
 
+// Relacionado ao processo de escrever na linha de comando no node js
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
+// Criar um socket novo e conectar ao servidor
 const client = new net.Socket()
 client.connect(4000, '0.0.0.0', () => {
-    console.log("Connected with the server!")
     rl.addListener('line', line => {
-        client.write(line)
+          client.write(line)
     })
 })
 
+// Logar as mensagens no console uma vez que enviadas pelo server
 client.on('data', data => {
-    console.log("Server:", data.toString());
+    console.log(data.toString());
  });
- 
+
 client.on('end', () => {
     console.log("Connection with the server has been killed")
 })
--- server.js
+++ server.js
@@ -1,23 +1,60 @@
 const net = require('net')
-const readline = require('readline')
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+// Espalha a mensagem pros demais users conectados
+var users = []
+const broadcast = msg => {
+  users.forEach(client => {
+    client.socket.write(msg)
+  })
+}
 
+// Processo que se passa na conexao e a gerencia
 const handleConnection = socket => {
-    console.log("Connected with the user!")
-    socket.on('end', () => {
-        console.log('User has disconnected')
-    })
+    // Mensagem introdutoria
+    socket.write("Welcome to our chat, please tell us your username")
+
+    // Caso receba dados do server
     socket.on('data', data => {
-        console.log("Client: ", data.toString())
+        // verifica indices de user, se ele nao estiver contido na lista, cria-se um objeto visando
+        // inclui-lo de modo que seu username e seu socket estejam guardados
+        userIndex = getIndexOfUser(socket)
+        if (userIndex == -1) {
+          users.push({'socket': socket, 'username': data.toString()})
+        } else {
+          broadcast(users[index]['username']+": "+data.toString())
+        }
     })
-    rl.addListener('line', line => {
-        socket.write(line)
+    socket.on('end', () => {
+        socket.write("User Disconnected")
+        userIndex = getIndexOfUser(socket)
+        users.splice(userIndex, 1)
     })
 }
 
+/* Funcao para encontrar o socket na array mais facilmente
+    Argumentos:
+        socketInfo (socket) = O socket que queremos achar na array
+
+    Retorna:
+        index (inteiro) = O indice do elemento que estamos procurando na array de users
+                          retorna -1 se nao estiver contido nela
+*/
+function getIndexOfUser(socketInfo) {
+    index = 0
+    stopped = false
+    while (!stopped) {
+      if (index == users.length) {
+        stopped = true
+        index = -1
+      } else if (users[index]['socket'] == socketInfo) {
+        stopped = true
+      } else {
+        index++
+      }
+    }
+    return index
+}
+
+// Cria servidor
 const server = net.createServer(handleConnection)
 server.listen(4000, '0.0.0.0')
